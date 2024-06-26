ded055ebeda5e708d34af9e106e2e92d41242f87
Anhar Solehudin <anhsbolic@gmail.com>
2024-06-26 08:21:17 +0700

    save injector progress

---
+++ injector.go
@@ -0,0 +1,39 @@
+//go:build wireinject
+// +build wireinject
+
+package main
+
+import (
+	"github.com/go-playground/validator/v10"
+	"github.com/google/wire"
+	"github.com/julienschmidt/httprouter"
+	"go-restful-fiber/app"
+	"go-restful-fiber/controller"
+	"go-restful-fiber/middleware"
+	"go-restful-fiber/repository"
+	"go-restful-fiber/service"
+	"net/http"
+)
+
+var categorySet = wire.NewSet(
+	repository.NewCategoryRepository,
+	wire.Bind(new(repository.CategoryRepository), new(*repository.CategoryRepositoryImpl)),
+	service.NewCategoryService,
+	wire.Bind(new(service.CategoryService), new(*service.CategoryServiceImpl)),
+	controller.NewCategoryController,
+	wire.Bind(new(controller.CategoryController), new(*controller.CategoryControllerImpl)),
+)
+
+func InitializedServer() *http.Server {
+	wire.Build(
+		app.NewDB,
+		validator.New,
+		wire.Bind(new(validator.Validate), new(*validator.Option)),
+		categorySet,
+		app.NewRouter,
+		wire.Bind(new(http.Handler), new(*httprouter.Router)),
+		middleware.NewAuthMiddleware,
+		NewServer,
+	)
+	return nil
+}
--- main.go
+++ main.go
@@ -1,35 +1,21 @@
 package main
 
 import (
-	"github.com/go-playground/validator/v10"
-	"go-restful-fiber/app"
-	"go-restful-fiber/controller"
-	"go-restful-fiber/helper"
 	"go-restful-fiber/middleware"
-	"go-restful-fiber/repository"
-	"go-restful-fiber/service"
 	"net/http"
 )
 
-func main() {
-	// Setup DB
-	db := app.NewDB()
-
-	// Setup Validator
-	validate := validator.New()
-
-	// Setup Category API
-	categoryRepository := repository.NewCategoryRepository()
-	categoryService := service.NewCategoryService(categoryRepository, db, validate)
-	categoryController := controller.NewCategoryController(categoryService)
-	router := app.NewRouter(categoryController)
-
-	// Setup Server
-	server := http.Server{
+func NewServer(authMiddleware *middleware.AuthMiddleware) *http.Server {
+	return &http.Server{
 		Addr:    "localhost:3000",
-		Handler: middleware.NewAuthMiddleware(router),
+		Handler: authMiddleware,
 	}
+}
 
-	err := server.ListenAndServe()
-	helper.PanicIfError(err)
+func main() {
+	//validatorOption := validator.Option()
+	//validate := validator.New()
+	//server := app.InitializeServer()
+	//err := server.ListenAndServe()
+	//helper.PanicIfError(err)
 }
